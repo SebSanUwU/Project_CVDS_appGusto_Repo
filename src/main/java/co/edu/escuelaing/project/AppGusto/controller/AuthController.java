e5b434ed9f274833711e4807c34f5265735e6a8a
Jefferson Mesa <mesajefer2@gmail.com>
2023-11-26 10:06:20 -0500

    delete all part of jwt

--- src/main/java/co/edu/escuelaing/project/AppGusto/controller/AuthController.java
+++ src/main/java/co/edu/escuelaing/project/AppGusto/controller/AuthController.java
@@ -1,8 +1,5 @@
 package co.edu.escuelaing.project.AppGusto.controller;
-import co.edu.escuelaing.project.AppGusto.demojwt.Auth.AuthResponse;
-import co.edu.escuelaing.project.AppGusto.demojwt.Auth.LoginRequest;
-import co.edu.escuelaing.project.AppGusto.demojwt.Auth.RegisterRequest;
-import co.edu.escuelaing.project.AppGusto.service.AuthService;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,15 +11,14 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final AuthService authService;
 
 
     @PostMapping(value = "login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
-        return ResponseEntity.ok(authService.login(request));
+    public void login(){
+
     }
     @PostMapping(value ="register" )
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
-        return ResponseEntity.ok(authService.register(request));
+    public void register(){
+
     }
 }
