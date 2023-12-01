36cf5a1156bbef94e2efedf5fc493e866749e2bd
SebSanUwU <juancamargo206@gmail.com>
2023-12-01 15:14:17 -0500

    Crear espacio de pruebas

---
+++ src/test/java/co/edu/escuelaing/project/AppGustoTest/AppGustoTest.java
@@ -0,0 +1,11 @@
+package co.edu.escuelaing.project.AppGustoTest;
+
+import co.edu.escuelaing.project.AppGusto.service.AdministradorService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AppGustoTest {
+
+}
