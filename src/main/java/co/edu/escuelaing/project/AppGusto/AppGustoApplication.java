f55836d529fff01eeda1ea19fd3c5b2572c3c1cf
SebSanUwU <juancamargo206@gmail.com>
2023-12-01 15:44:03 -0500

    Correción de Configuracion que no se estaba usando, falta limpieza de codigo en servicios

--- src/main/java/co/edu/escuelaing/project/AppGusto/AppGustoApplication.java
+++ src/main/java/co/edu/escuelaing/project/AppGusto/AppGustoApplication.java
@@ -1,20 +1,10 @@
 package co.edu.escuelaing.project.AppGusto;
 
-
-import co.edu.escuelaing.project.AppGusto.model.Configuration;
-
-import co.edu.escuelaing.project.AppGusto.service.ConfigurationService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class AppGustoApplication {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppGustoApplication.class, args);
